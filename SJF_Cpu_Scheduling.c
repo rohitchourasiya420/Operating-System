d4899b13cc859583507c62602d7d0bf5126645e1
Saurabh Kumar <kumarsaurabh8286@gmail.com>
2023-05-02 17:30:22 +0530

    updated

---
+++ SJF_Cpu_Scheduling.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#define max 10
+// void bubble_sort(int A , int n);
+
+int main()
+{
+   // int pro[4]={0,1,2,3}; // for No. of process
+    int AT[max]={0}; //For Arival time
+    int BT[max]; //For Burst time
+    int CT[max]; //For completion time
+    int WT[max]; //For Waiting time
+    int TAT[max]; //For turn around time
+    int i,n;
+    printf("\nEnter total number of processes:\n");
+    scanf("%d",&n);
+
+
+    printf("\nArival time for each  process is :\n");
+    for(i=0 ; i<n ; i++)
+      printf("%d ",AT[i]);
+
+    printf("\nEnter Burst time for each  process:\n");
+    for(i=0;i<n;i++)
+      scanf("%d",&BT[i]);
+
+      //Sort BT
+       for(i=0 ; i<n ; i++){
+            for(int j=i+1;j<4;j++){
+               if(BT[i]>=BT[j])
+                  {
+                     int temp=BT[i];
+                     BT[i]=BT[j];
+                     BT[j]=temp;
+                  }
+               }
+            }
+
+   printf("\n BT After sort\n");
+   for(i=0;i<n;i++){
+      printf("%d ",BT[i]);
+   }
+
+    int GC[4]={0}; //Gantt Chart
+   
+   //printf("\n");
+    
+    for(i=0 ; i<n ;i++)
+    { 
+      if(i==0)
+         GC[i] = BT[i];
+      else 
+       GC[i]=GC[i-1]+BT[i];
+    }
+
+   printf("\nValue inside Gantt Chart :\n");
+   for(int i=0;i<n;i++)
+   {
+      printf("%d ",GC[i]);
+   }
+
+   for(i=0;i<n;i++){  //Time inside gantt chart is CT
+      CT[i]=GC[i];
+   }
+
+   printf("\nTime inside Completion Time (CT):\n");
+   for(i=0;i<n;i++)
+   {
+      printf("%d ",CT[i]);
+   }
+   
+   int TAT_sum=0;
+    
+   for(i=0;i<n;i++){
+      TAT[i]=CT[i]-AT[i];
+      TAT_sum+=TAT[i];
+   }
+   float avg_TAT = (float)TAT_sum/4.0;
+
+   printf("\nTime inside TAT :\n");
+   for(int i=0;i<n;i++)
+   {
+      printf("%d ",TAT[i]);
+   }
+   
+   int WT_sum=0;
+   for(i=0;i<n;i++){
+      WT[i]=TAT[i]-BT[i];
+      WT_sum+=WT[i];
+   }
+   float avg_WT = (float)WT_sum/4.0;
+
+   printf("\nTime inside WT :\n");
+   for(int i=0;i<n;i++)
+   {
+      printf("%d ",WT[i]);
+   }
+   
+   printf("\nAverage TAT is : %.2f ",avg_TAT);
+   printf("\nAverage WT is : %.2f ",avg_WT);
+           
+  return 0;
+}
