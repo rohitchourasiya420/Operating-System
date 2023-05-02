aff14b5eb5fb00164c9f4279c17d51e5af887ff1
Rohit chourasiya <113618383+rohitchourasiya420@users.noreply.github.com>
2023-05-02 22:52:49 +0530

    Update FCFS_Disk_Scheduling.c
    in this program n variable is replaced by a so, it will be run program more efficent

--- FCFS_Disk_Scheduling.c
+++ FCFS_Disk_Scheduling.c
@@ -3,15 +3,15 @@
 int main() {
    
     int total_head_movement=0;
-    int n; ///To take total no. of cylinder
+    int a; ///To take total no. of cylinder
     
     printf("\nEnter the no. of cylinders to enter in Request Queue : ");
-    scanf("%d",&n);
+    scanf("%d",&a);
     
-    int request_queue[n];
+    int request_queue[a];
     printf("\n Enter the cylinders no. in Request queue :\n");
     
-    for(int i=0 ; i<n ; i++){
+    for(int i=0 ; i<a ; i++){
         scanf("%d",&request_queue[i]);
     }
     
@@ -19,7 +19,7 @@ int main() {
     printf("\nEnter the initial position of RW head: ");
     scanf("%d",&initial_pos);
     
-    for(int i=0;i<n;i++){
+    for(int i=0;i<a;i++){
         total_head_movement += abs(initial_pos - request_queue[i]);
         initial_pos = request_queue[i];
     }
